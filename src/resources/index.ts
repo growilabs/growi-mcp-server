ac60360f0fc6d271cdef23abd02cf69a075cf4f7
Yuki Takei <yuki@weseek.co.jp>
2025-06-10 12:24:47 +0000

    remove legacy implementations

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,7 +1,4 @@
 import type { FastMCP } from 'fastmcp';
-import { registerAncestorsChildrenResource } from './ancestorsChildren';
-import { registerGetExternalAccountsResource } from './externalAccounts';
-import { registerMeResource } from './me';
 import { registerPageResource } from './page';
 import { registerPageInfoResource } from './page/info';
 import { registerPageTagsResource } from './tag';
@@ -9,8 +6,5 @@ import { registerPageTagsResource } from './tag';
 export async function loadResources(server: FastMCP): Promise<void> {
   registerPageResource(server);
   registerPageInfoResource(server);
-  registerAncestorsChildrenResource(server);
   registerPageTagsResource(server);
-  registerMeResource(server);
-  registerGetExternalAccountsResource(server);
 }
