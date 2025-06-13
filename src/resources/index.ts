4f3eb667c05587ffec7139cde98cf625c3dfdc69
Yuki Takei <yuki@weseek.co.jp>
2025-06-13 12:40:56 +0000

    implement recent resource

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,10 +1,12 @@
 import type { FastMCP } from 'fastmcp';
 import { registerPageResource } from './page';
 import { registerPageInfoResource } from './page/info';
+import { registerRecentPagesResource } from './recent';
 import { registerPageTagsResource } from './tag';
 
 export async function loadResources(server: FastMCP): Promise<void> {
   registerPageResource(server);
   registerPageInfoResource(server);
   registerPageTagsResource(server);
+  registerRecentPagesResource(server);
 }
