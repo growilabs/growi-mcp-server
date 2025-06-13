577e36e488fbb0df9c481c849449c740a0cb56cc
Yuki Takei <yuki@weseek.co.jp>
2025-06-13 12:58:03 +0000

    remove tag resource

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -4,12 +4,10 @@ import { registerPageInfoResource } from './page/info';
 import { registerPageListingChildrenResource } from './pageListingChildren';
 import { registerPageListingRootResource } from './pageListingRoot';
 import { registerRecentPagesResource } from './recent';
-import { registerPageTagsResource } from './tag';
 
 export async function loadResources(server: FastMCP): Promise<void> {
   registerPageResource(server);
   registerPageInfoResource(server);
-  registerPageTagsResource(server);
   registerRecentPagesResource(server);
   registerPageListingRootResource(server);
   registerPageListingChildrenResource(server);
