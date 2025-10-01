e26b4a2ba2565eef042507f2adcfce62ee31e6cf
Shun Miyazawa <m0zurillex@gmail.com>
2025-10-01 09:25:58 +0000

    rm obsoleted files

--- src/tools/commons/client-utils.ts
+++
@@ -1,6 +0,0 @@
-import { growiClientManager } from '../../commons/api/growi-client-manager';
-
-export const setGrowiClient = async (appName?: string): Promise<void> => {
-  const targetAppName = appName || growiClientManager.getDefaultAppName();
-  await growiClientManager.setGlobalAxiosClient(targetAppName);
-};
